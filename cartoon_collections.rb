d3c076c6b6f9878abc7faae6fd33ea3873c1f50d
Marc Cavazos <marcavazos19@icloud.com>
2022-02-22 17:11:51 -0500

    commit tree e7661e3caf18609e632b8f25b2105963b8bb7ec0

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves dwarves# code an argument here
   # Your code here
+  dwarves.each.with_index { |dwarf, i| puts "#{i + 1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet calls# code an argument here
   # Your code here
+  calls.map { |call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls calls# code an argument here
   # Your code here
+  calls.any? { |call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese ingredients# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  ingredients.find do |i| 
+    cheese_types.include?(i)
+  end
 end
